1f12066e06a325db0a55d8cd7d982196b43e9055
Alexander Zakharchuk <51047596+fuetser@users.noreply.github.com>
2023-07-24 16:27:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+
+
 for i in range(1, 11):
   with open(f"test{i}.txt", "w") as fo:
     print(i, file=fo)
+
+os.system("touch out.log && mkdir newdir && cd newdir && touch in.log && touch in.txt")
