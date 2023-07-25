9ae6e980d7ecd5bece6ab6fc62dc59cf11a3cbb8
Alexander Zakharchuk <51047596+fuetser@users.noreply.github.com>
2023-07-25 14:48:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import os
 
 
-for i in range(1, 11):
+os.mkdir("artifacts")
+os.mkdir("logs")
+
+os.chdir("artifacts")
+for i in range(1, 6):
   with open(f"test{i}.txt", "w") as fo:
     print(i, file=fo)
 
-os.system("touch out.log && mkdir newdir && cd newdir && touch in.log && touch in.txt")
+os.mkdir("inner")
+os.chdir("inner")
+for i in range(6, 11):
+  with open(f"test{i}.txt", "w") as fo:
+    print(i, file=fo)
+
+os.chdir("..")
+os.chdir("..")
+os.chdir("logs")
+
+for i in range(1, 6):
+  with open(f"log{i}.log", "w") as fo:
+    print(i, file=fo)
+
+os.mkdir("inner")
+os.chdir("inner")
+for i in range(6, 11):
+  with open(f"log{i}.log", "w") as fo:
+    print(i, file=fo)
