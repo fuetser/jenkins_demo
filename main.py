4c5865c94795e4df67f001134c8e2c999c5e0679
Alexander Zakharchuk <51047596+fuetser@users.noreply.github.com>
2023-07-28 15:50:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 
 
 data = []
-with open("/dev/urandom", "rb") as fi:
-  chunck = fi.read(1024)
-  data.append(chunck)
-  time.sleep(0.5)
+while True:
+    with open("/dev/urandom", "rb") as fi:
+        chunck = fi.read(1024)
+        data.append(chunck)
+    time.sleep(0.5)
