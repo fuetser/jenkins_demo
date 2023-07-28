78bace89f845bde9aa488d5e74ce46e84e3a53d5
Alexander Zakharchuk <51047596+fuetser@users.noreply.github.com>
2023-07-28 15:48:10 +0300

    Reading from dev/urandom

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-import os
+import time
 
 
-os.mkdir("artifacts")
-os.mkdir("logs")
-
-os.chdir("artifacts")
-for i in range(1, 6):
-  with open(f"test{i}.txt", "w") as fo:
-    print(i, file=fo)
-
-for i in range(1, 6):
-  with open(f"script{i}.json", "w") as fo:
-    print(i, file=fo)
-
-os.mkdir("inner")
-os.chdir("inner")
-for i in range(6, 11):
-  with open(f"test{i}.txt", "w") as fo:
-    print(i, file=fo)
-
-os.chdir("..")
-os.chdir("..")
-os.chdir("logs")
-
-for i in range(1, 6):
-  with open(f"log{i}.log", "w") as fo:
-    print(i, file=fo)
-
-os.mkdir("inner")
-os.chdir("inner")
-for i in range(6, 11):
-  with open(f"log{i}.log", "w") as fo:
-    print(i, file=fo)
+data = []
+with open("/dev/urandom", "rb") as fi:
+  chunck = fi.read(1024)
+  data.append(chunck)
+  time.sleep(0.5)
