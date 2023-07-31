73fc001ef1ec2ad2e7de51cf7eb2249bff1695f4
Alexander Zakharchuk <51047596+fuetser@users.noreply.github.com>
2023-07-31 18:09:05 +0300

    Lowered CPU and RAM usage

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 
 def consume_cpu():
     # Perform some CPU-intensive operation
-    for _ in range(10000000):
+    for _ in range(1000000):
         random.random()
 
 def consume_memory():
     # Allocate memory and hold it for some time
-    memory_hog = ['Hog'] * (1024 * 1024 * 1024)  # Allocating 100 MB
+    memory_hog = ['Hog'] * (100 * 1024 * 1024)  # Allocating 100 MB
     time.sleep(5)  # Hold memory for 5 seconds
     del memory_hog
 
