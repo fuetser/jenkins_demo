3d9634a253784a3a1f2f392c5addde2c3e182cc4
Alexander Zakharchuk <51047596+fuetser@users.noreply.github.com>
2023-08-02 17:46:02 +0300

    Update stress.py

--- stress.py
+++ stress.py
@@ -33,7 +33,7 @@ y = titanic['survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize the CatBoost classifier
-model = CatBoostClassifier(iterations=1000,  # Number of boosting iterations
+model = CatBoostClassifier(iterations=10000,  # Number of boosting iterations
                            learning_rate=0.1,  # Learning rate for gradient boosting
                            depth=6,  # Maximum depth of the tree
                            loss_function='Logloss',  # Loss function for binary classification
